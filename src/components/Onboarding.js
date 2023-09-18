c443fd572aef8fd4c8ef6f193345a43d4821b3bd
Nando Lutgerink <nando.lutgerink@gmail.com>
2023-09-18 10:08:06 +0200

    demo

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Onboarding() {
   return (
     <div className="onboarding">
       <h1>Timer</h1>
+      welcome to the timer app
     </div>
-  )
+  );
 }
 
-export default Onboarding
+export default Onboarding;
--- src/hooks/useCountDown.js
+++ src/hooks/useCountDown.js
@@ -2,7 +2,10 @@ import { useState, useEffect } from "react";
 
 const useCountdown = ({ timeInMilliSeconds, countDownStarted, countDownTime }) => {
 
-    const [countDownInMilliSeconds, setCountDown] = useState(countDownTime - new Date().getTime());
+const [countDownInMilliSeconds, setCountDown] = useState(countDownTime - new Date().getTime());
+console.log('timeInMilliSeconds:', timeInMilliSeconds);
+console.log('countDownStarted:', countDownStarted);
+console.log('countDownTime:', countDownTime);
 
     useEffect(() => {
         let interval
@@ -22,15 +25,17 @@ const useCountdown = ({ timeInMilliSeconds, countDownStarted, countDownTime }) =
         if (!countDownStarted) { setCountDown(0); return }
     }, [countDownStarted])
 
-    return getReturnValues(countDownInMilliSconds);
+    return getReturnValues(countDownInMilliSeconds);
 };
 
 
 const getReturnValues = (countDown) => {
     // calculate the time left
-    var minutes = Math.floor(countDown / (60 * ));
-    var seconds = parseInt(((countDown % (60 * )) / 1000).toFixed(0));
+    var minutes = Math.floor(countDown / (60 * 1000));
+    var seconds = parseInt(((countDown % (60 * 1000)) / 1000).toFixed(0));
+    console.log('minutes:', minutes);
+    console.log('seconds:', seconds);
     return [minutes, seconds];
-};
+  };
 
-export {  };
+  export { useCountdown };
